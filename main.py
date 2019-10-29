98f5d88710a4a11e8bf0ef3929ca654457df65f4
SrujanaN <srujana.n93@gmail.com>
2019-10-29 16:45:46 -0500

    Read from csv and add into pd

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ df_meta = pd.read_csv(table_meta)
 
 product_ids = dfa.asin.tolist()
 product_ids_meta = df_meta.Product_ID
-print(product_ids_meta)
-print(len(product_ids))
+# print(product_ids_meta)
+# print(len(product_ids))
+cat_data_train = []
+for x in df_meta:
+    count = 0
+    rating = 0
+    # print(x)
+    for y in dfa:
+        print(dfa[y][2])
+        if df_meta[x][0] == dfa[y][0]:
+            # rating = rating + dfa[y][2]
+            count = count + 1
+    # average = rating / count
+
+
+
+
+# average = dfa[df_meta["Product_ID"] == dfa["asin"]].sum()["overall"]
+# print(average)
+
+# average rating
 
