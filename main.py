d132d9908eeb985113ce984d45d8dabfb820c1b5
SrujanaN <srujana.n93@gmail.com>
2019-10-29 12:37:47 -0500

    Read from csv and add into pd

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import numpy as np
-import json
-import sys
 import pandas as pd
+import collections
+import csv
 
-table_a = 'CDs_and_Vinyl_5'
-table_b = 'CDs_And_Vinyl_meta_5'
+table_a = 'CDs_and_Vinyl_5.csv'
+table_meta = 'CDs_And_Vinyl_meta_5.csv'
 
 dfa = pd.read_csv(table_a)
-dfb = pd.read_csv(table_b)
+df_meta = pd.read_csv(table_meta)
+
+product_ids = dfa.asin.tolist()
+product_ids_meta = df_meta.Product_ID
+print(product_ids_meta)
+print(len(product_ids))
+
