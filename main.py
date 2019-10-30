feae9aa2b4210a757db71849481a59d8558b123c
SrujanaN <srujana.n93@gmail.com>
2019-10-30 02:56:50 -0500

    pyplot to show rating for every customer, ordered by rating

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 import pandas as pd
 import numpy as np
-import collections
-import csv
-import sys
-import os
+import plotly.graph_objects as go
+from collections import OrderedDict
 
 table_a = 'CDs_and_Vinyl_5.csv'
 table_meta = 'CDs_And_Vinyl_meta_5.csv'
-
-
-# def loadData(fileName):
-#   productId = []
-#   rating = []
-#   with open(fileName) as csvfile:
-#       readCSV = csv.reader(csvfile, delimiter=',')
-#       for row in readCSV:
-#           product = row[0]
-#           rate = row[2]
-#           productId.append(product)
-#           rating.append(rate)
-#   return np.array(productId), np.array(rating), np.array(readCSV)
-#
-#
-# productId, rating, tab = loadData(table_a)
-# productIdMeta, price, tabMeta = loadData(table_meta)
-# # print(tab[0])
 
 
 dfa = pd.read_csv(table_a)
@@ -32,43 +12,36 @@ df_meta = pd.read_csv(table_meta)
 np_dfa = np.array(dfa)
 np_df_meta = np.array(df_meta)
 col = np_dfa[:, 2]
-print(col)
+product_rating = {}
+count = 0
 for x in np_df_meta:
-    count = 0
-    rating = 0
-    col = np_dfa[:, 2]
-    # print(col)
-#     for y in np_dfa:
-#         if x[0] == y[0]:
-#             count = count + 1
-#             rating = rating + y[2]
-#     print(rating/count)
-#     print(x[0])
-# print(np.sum(np_dfa[:, 2]))
-# average = np_dfa[:, np_df_meta["Product_ID"] == dfa["asin"]].sum()["overall"]
-#
-# product_ids = dfa.asin.tolist()
-# product_ids_meta = df_meta.Product_ID
-# # print(product_ids_meta)
-# # print(len(product_ids))
-# cat_data_train = []
-# for x in df_meta:
-#     count = 0
-#     rating = 0
-#     # print(x)
-#     for y in dfa:
-#         print(dfa[y][2])
-#         if df_meta[x][0] == dfa[y][0]:
-#             # rating = rating + dfa[y][2]
-#             count = count + 1
-#     # average = rating / count
+    col = np_dfa[np_dfa[:, 0] == x[0], 2]
+    product_rating.update({x[0]: sum(col)/len(col)})
+
+d_sorted_by_value = dict(OrderedDict(sorted(product_rating.items(), key=lambda x: x[1])))
+
+# product_rating = np.array(sorted(product_rating.values()))
+# d_sorted_keys = list(d_sorted_by_value.keys())
+# types1 = [type(k) for k in d_sorted_by_value.keys()]
+# print(type(d_sorted_keys[0]))
+# print(type(np_dfa[0,0]))
+# if type(d_sorted_keys[0]) == type(np_dfa[0,0]):
+#     print("same")
+# key_list = []
+# for k in d_sorted_keys:
+#     key_list.append(np_df_meta[np_df_meta[:, 0] == k, 0])
+# print(d_sorted_by_value)
+# dd = np.array(d_sorted_keys)
+# print(dd[:])
+# print(len(d_sorted_keys))
 
 
+# Create figure
 
+fig = go.Figure()
 
+fig.add_trace(
+    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(d_sorted_by_value.values())))
 
-# average = dfa[df_meta["Product_ID"] == dfa["asin"]].sum()["overall"]
-# print(average)
-
-# average rating
+fig.show()
 
