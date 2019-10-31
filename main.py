fb5f2ee4bb4cbe4213fb3d392abc6e0f7337b7c2
SrujanaN <srujana.n93@gmail.com>
2019-10-30 22:25:23 -0500

    Output folder, including output file

--- main.py
+++ main.py
@@ -7,18 +7,13 @@ from plotly.subplots import make_subplots
 table_a = 'CDs_and_Vinyl_5.csv'
 table_meta = 'CDs_And_Vinyl_meta_5.csv'
 
-
 dfa = pd.read_csv(table_a)
 df_meta = pd.read_csv(table_meta)
 np_dfa = np.array(dfa)
 np_df_meta = np.array(df_meta)
 product_rating = {}
 
-count = 0
 for x in np_df_meta:
-    # if count == 2000:
-    #     break
-    # count = count + 1
     col = np_dfa[np_dfa[:, 0] == x[0], 2]
     product_rating.update({x[0]: sum(col)/len(col)})
 
@@ -48,5 +43,5 @@ fig.update_xaxes(title_text="<b>Product ID's</b>")
 fig.update_yaxes(title_text="<b>Average rating/product </b>", secondary_y=False)
 fig.update_yaxes(title_text="<b>Price</b>", secondary_y=True)
 
-fig.savefig('dc2-1.png')
+# fig.savefig('dc2-1.png')
 fig.show()
