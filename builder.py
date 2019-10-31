c38126537c8e3845bb0e14b219daa20943bf9f00
SrujanaN <srujana.n93@gmail.com>
2019-10-30 20:16:22 -0500

    pyplot with two lines, trying for dual axis

--- builder.py
+++ builder.py
@@ -2,6 +2,8 @@ import plotly.graph_objects as go
 
 import pandas as pd
 
+from ipywidgets import interactive, HBox, VBox, widgets, interact
+
 # Load data
 df = pd.read_csv("finance-charts-apple.csv")
 df.columns = [col.replace("AAPL.", "") for col in df.columns]
@@ -22,7 +24,7 @@ fig.update_layout(
     xaxis=go.layout.XAxis(
         rangeselector=dict(
             buttons=list([
-                dict(count=1,
+                dict(count=2,
                      label="1m",
                      step="month",
                      stepmode="backward"),
@@ -47,5 +49,31 @@ fig.update_layout(
         type="date"
     )
 )
+
+# find the range of the slider.
+xx, yy = fig['layout']['xaxis']['range']
+
+xmax, xmin = 0, 0
+# create FigureWidget from fig
+f = go.FigureWidget(data=fig.data, layout=fig.layout)
+
+slider = widgets.FloatRangeSlider(
+    min=xmin,
+    max=xmax,
+    step=(xmax - xmin) / 1000.0,
+    readout=False,
+    description='Time')
+slider.layout.width = '800px'
+
+
+# our function that will modify the xaxis range
+def update_range(y):
+    f.layout.xaxis.range = [y[0], y[1]]
+
+
+# display the FigureWidget and slider with center justification
+vb = VBox((f, interactive(update_range, y=slider)))
+vb.layout.align_items = 'center'
+vb
 
 fig.show()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from collections import OrderedDict
+import matplotlib.pyplot as plt
 
 table_a = 'CDs_and_Vinyl_5.csv'
 table_meta = 'CDs_And_Vinyl_meta_5.csv'
@@ -13,35 +14,71 @@ np_dfa = np.array(dfa)
 np_df_meta = np.array(df_meta)
 col = np_dfa[:, 2]
 product_rating = {}
+
 count = 0
 for x in np_df_meta:
+    if count == 2000:
+        break
+    count = count + 1
     col = np_dfa[np_dfa[:, 0] == x[0], 2]
     product_rating.update({x[0]: sum(col)/len(col)})
 
+
 d_sorted_by_value = dict(OrderedDict(sorted(product_rating.items(), key=lambda x: x[1])))
+keyyy = list(d_sorted_by_value.keys())
+d_price = np_df_meta[np_df_meta[:, 0] == d_sorted_by_value.keys(), 2]
 
-# product_rating = np.array(sorted(product_rating.values()))
-# d_sorted_keys = list(d_sorted_by_value.keys())
-# types1 = [type(k) for k in d_sorted_by_value.keys()]
-# print(type(d_sorted_keys[0]))
-# print(type(np_dfa[0,0]))
-# if type(d_sorted_keys[0]) == type(np_dfa[0,0]):
-#     print("same")
-# key_list = []
-# for k in d_sorted_keys:
-#     key_list.append(np_df_meta[np_df_meta[:, 0] == k, 0])
-# print(d_sorted_by_value)
-# dd = np.array(d_sorted_keys)
-# print(dd[:])
-# print(len(d_sorted_keys))
+print(keyyy)
 
 
-# Create figure
-
+# create graph
 fig = go.Figure()
 
-fig.add_trace(
-    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(d_sorted_by_value.values())))
 
+fig.add_trace(
+    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(d_sorted_by_value.values()), mode='lines', name='Average rating'
+               ))
+
+fig.add_trace(
+    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(np_df_meta[:, 2]), mode='lines', name='Price'))
+
+# fig, ax = plt.subplots(constrained_layout=True)
+# x = list(d_sorted_by_value.keys())
+# y = list(d_sorted_by_value.values())
+# ax.plot(x, y)
+# ax.set_xlabel('angle [degrees]')
+# ax.set_ylabel('signal')
+# ax.set_title('Sine wave')
+#
+#
+# def deg2rad(x):
+#     return x * np.pi / 180
+#
+#
+# def rad2deg(x):
+#     return x * 180 / np.pi
+#
+#
+# def meta(x):
+#     return np_df_meta[:, 2]
+#
+#
+# secax = ax.secondary_yaxis('right', function=(meta, np_df_meta))
+# secax.set_ylabel('angle [rad]')
+# plt.show()
+#
 fig.show()
 
+
+# twinx
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+t = np.array(list(d_sorted_by_value.items()))
+ax1.plot(t[:, 0], t[:, 1], 'b-')
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('exp')
+
+ax2 = ax1.twinx()
+# ax2.plot(t[:, 0], t[:, 1], 'r.')
+ax2.set_ylabel('sin')
+plt.show()
