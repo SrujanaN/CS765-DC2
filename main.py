d844e843fb08ae11b793e9aedbf95aad2e045dd8
SrujanaN <srujana.n93@gmail.com>
2019-10-30 21:32:21 -0500

    pyplot with dual axis, vis-1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from collections import OrderedDict
-import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
 
 table_a = 'CDs_and_Vinyl_5.csv'
 table_meta = 'CDs_And_Vinyl_meta_5.csv'
@@ -12,73 +12,41 @@ dfa = pd.read_csv(table_a)
 df_meta = pd.read_csv(table_meta)
 np_dfa = np.array(dfa)
 np_df_meta = np.array(df_meta)
-col = np_dfa[:, 2]
 product_rating = {}
 
 count = 0
 for x in np_df_meta:
-    if count == 2000:
-        break
-    count = count + 1
+    # if count == 2000:
+    #     break
+    # count = count + 1
     col = np_dfa[np_dfa[:, 0] == x[0], 2]
     product_rating.update({x[0]: sum(col)/len(col)})
 
-
 d_sorted_by_value = dict(OrderedDict(sorted(product_rating.items(), key=lambda x: x[1])))
 keyyy = list(d_sorted_by_value.keys())
-d_price = np_df_meta[np_df_meta[:, 0] == d_sorted_by_value.keys(), 2]
 
-print(keyyy)
-
+d_price = {}
+for k in d_sorted_by_value.keys():
+    col = np_df_meta[np_df_meta[:, 0] == k, 2]
+    d_price.update({k: sum(col)})
 
 # create graph
-fig = go.Figure()
+fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 
 fig.add_trace(
     go.Scatter(x=list(d_sorted_by_value.keys()), y=list(d_sorted_by_value.values()), mode='lines', name='Average rating'
-               ))
+               ), secondary_y=False,)
 
 fig.add_trace(
-    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(np_df_meta[:, 2]), mode='lines', name='Price'))
+    go.Scatter(x=list(d_sorted_by_value.keys()), y=list(d_price.values()), name='Price'),
+    secondary_y=True,)
+# Set x-axis title
+fig.update_xaxes(title_text="<b>Product ID's</b>")
 
-# fig, ax = plt.subplots(constrained_layout=True)
-# x = list(d_sorted_by_value.keys())
-# y = list(d_sorted_by_value.values())
-# ax.plot(x, y)
-# ax.set_xlabel('angle [degrees]')
-# ax.set_ylabel('signal')
-# ax.set_title('Sine wave')
-#
-#
-# def deg2rad(x):
-#     return x * np.pi / 180
-#
-#
-# def rad2deg(x):
-#     return x * 180 / np.pi
-#
-#
-# def meta(x):
-#     return np_df_meta[:, 2]
-#
-#
-# secax = ax.secondary_yaxis('right', function=(meta, np_df_meta))
-# secax.set_ylabel('angle [rad]')
-# plt.show()
-#
+# Set y-axes titles
+fig.update_yaxes(title_text="<b>Average rating/product </b>", secondary_y=False)
+fig.update_yaxes(title_text="<b>Price</b>", secondary_y=True)
+
+fig.savefig('dc2-1.png')
 fig.show()
-
-
-# twinx
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-t = np.array(list(d_sorted_by_value.items()))
-ax1.plot(t[:, 0], t[:, 1], 'b-')
-ax1.set_xlabel('time (s)')
-ax1.set_ylabel('exp')
-
-ax2 = ax1.twinx()
-# ax2.plot(t[:, 0], t[:, 1], 'r.')
-ax2.set_ylabel('sin')
-plt.show()
